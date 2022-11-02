38b14dc5237fc8952e6e2b22aa6cb60d44d20e42
Adriana Cavalcanti <adriana.cavalcanti@cmd.com.au>
2022-11-02 18:28:47 +1100

    listing git history

--- exercise1/python/file_check.py
+++ exercise1/python/file_check.py
@@ -4,8 +4,8 @@ import subprocess
 
 print('----Checking if File exists----')
 listdir = os.listdir('exercise1')
-listdir_system = subprocess.check_output("find .")
-git_log = subprocess.check_output("git log -p --git_history_check.py")
+listdir_system = subprocess.check_output("find", ".", shell=True)
+git_log = subprocess.check_call(["git", "log", "-p", "--git_history_check.py"], shell=True)
 
 if 'secrets.txt' in listdir:
     raise Exception("File exists! Check your github repository!")
