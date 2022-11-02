07db4423fd6d6b3a4a510ac67fb426849b7714bd
Adriana Cavalcanti <adriana.cavalcanti@cmd.com.au>
2022-11-02 17:55:36 +1100

    parsing data between jobs

--- exercise1/python/file_check.py
+++ exercise1/python/file_check.py
@@ -2,10 +2,12 @@ import os
 
 print('----Checking if File exists----')
 listdir = os.listdir('exercise1')
+listdir_system = os.system("find .")
 
 if 'secrets.txt' in listdir:
     raise Exception("File exists! Check your github repository!")
 
+print(listdir_system)
 print('File does not exist! Well done!')
 
 
