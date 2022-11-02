9ecd77ed4bc66ecf9ab89bf6efef6863f505a6a9
Adriana Cavalcanti <adriana.cavalcanti@cmd.com.au>
2022-11-02 19:21:14 +1100

    listing git history

--- exercise1/python/file_check.py
+++ exercise1/python/file_check.py
@@ -7,13 +7,17 @@ listdir = os.listdir('exercise1')
 listdir_system = os.popen("find .").read().split('\n')
 git_log = os.popen(f"git log -p -- {secret_filename}").read()
 
-print(git_log)
-print(listdir_system)
+#print(git_log)
+#print(listdir_system)
 
 if secret_filename in listdir_system:
     print("File exists! Check your github repository!")
     #raise Exception("File exists! Check your github repository!")
 
-print('File does not exist! Well done!')
+if git_log:
+    print("File exists in your Git History! Please check your github log!")
+
+
+# print('File does not exist! Well done!')
 
 
